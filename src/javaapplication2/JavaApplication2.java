373a0e25807bc9a872687b2678377ba802f07e25
c0633176 <c0633176@SB-N208-30.lambton.on.ca>
2015-01-13 09:32:54 -0500

    first one

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaapplication2;
+
+/**
+ *
+ * @author c0633176
+ */
+public class JavaApplication2 {
+
+   
+    public static void main(String[] args) {
+    double purp=132.85;
+    double st=(13*purp)/100;
+    double tt=purp+st;
+    System.out.println("purchase price"+purp);
+    System.out.println("total purchase price="+tt);   
+    System.out.println("tax="+st);
+    }
+    
+}
